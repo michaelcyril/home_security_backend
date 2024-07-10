629ac352ab8134ab20b4053b979ff54a3908b1f8
michaelcyril <michaelcyril71@gmail.com>
2024-07-10 12:22:10 +0300

    socket

--- HomeSecurity/routing.py
+++ HomeSecurity/routing.py
@@ -6,14 +6,11 @@ from sensor_management.consumer import AlertConsumer
 
 
 application = ProtocolTypeRouter({
-    # Empty
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
-            URLRouter(
+                URLRouter(
                 [
-                    # SALESMAN MAP ROOM
-                    re_path(r"^salesman_map_room/(?P<alert_id>)", AlertConsumer.as_asgi()),
-
+                    re_path(r"^alert-room/(?P<alert_id>)", AlertConsumer.as_asgi()),
                 ]
             )
         )
--- HomeSecurity/settings.py
+++ HomeSecurity/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'channels',
     "corsheaders",
     'rest_framework',
